e5d8d7d499c38952a51dc63b7f050e47fbb3476c
Kevin Rattai <krattai@gmail.com>
2015-08-09 16:03:17 -0500

    Added mosquitto install

---
+++ src/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# installs noo-ebs
+#
+# Copyright (C) 2015 Uvea I. S., Kevin Rattai
+#
+# Obviously, in this first instance of noo-ebs for test and
+# production, this script installs MQTT
+#
+# The goal is to eventually move to, or at least add as a
+# supplemental, a peer to peer message queue system
+#
+
+sudo apt-get install mosquitto python-mosquitto mosquitto-clients
