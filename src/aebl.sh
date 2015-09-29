c63d028581ad04d887a5c9342f401e2b7b00241b
Kevin Rattai <krattai@gmail.com>
2015-09-29 00:20:48 -0500

    Added noo-ebs installation script code

--- src/aebl.sh
+++ src/aebl.sh
@@ -10,8 +10,18 @@
 # supplemental, a peer to peer message queue system
 #
 
+# patch system now seeking network info in /run/shm
+TEMP_DIR="~/tempdir"
+
 sudo apt-get update
 
 sudo apt-get -y install openssh-server samba samba-common-bin libnss-mdns lsof
 
-sudo apt-get -y install mosquitto python-mosquitto mosquitto-clients dnsutils
+cd ~
+mkdir ${TEMP_DIR}
+
+# get noo-ebs installer and run it
+wget -N -nd -w 3 -P ${TEMP_DIR} --limit-rate=50k "https://raw.githubusercontent.com/krattai/noo-ebs/master/src/install.sh"
+chmod 777 ${TEMP_DIR}/install.sh
+${TEMP_DIR}/./install.sh
+rm ${TEMP_DIR}/install.sh
