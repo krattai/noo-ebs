62a3fd75c29d03476cd5a4b674f9c60a4a1bbfb6
Kevin Rattai <krattai@gmail.com>
2015-12-30 01:54:26 -0600

    Updated pub.sh for better loop control and hostname response

--- ref_code/mqtt/pub.sh
+++ ref_code/mqtt/pub.sh
@@ -2,12 +2,12 @@
 
 i="0"
 
-while [ $i -lt 9999 ]
+while [ $i -lt 1 ]
 do
+hostn=$(cat /etc/hostname)
 ext_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # mosquitto_pub -d -t hello/world -m "$(date) : irot LdB, online. IP is $ext_ip" -h "uveais.ca"
-mosquitto_pub -d -t hello/world -m "$(date) : device IP $ext_ip is online." -h "uveais.ca"
-i=$[$i+1]
+mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn device IP $ext_ip is online." -h "2604:8800:100:19a::2"
+# i=$[$i+1]
 sleep 300
 done
-
