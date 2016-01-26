e958ee8fe0708e1a03f6f8da5b3643374195e5f9
Kevin Rattai <krattai@gmail.com>
2016-01-26 16:53:56 -0600

    Added test / example python client to subscribe and publish to broker

---
+++ ref_code/mqtt/clpi.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+
+# Unknown license, code found here:  http://stackoverflow.com/questions/31775450/publish-and-subscribe-with-paho-mqtt-client
+
+# reference to MQTT python found here: http://mosquitto.org/documentation/python/
+
+
+
+message = 'ON'
+def on_connect(mosq, obj, rc):
+    mqttc.subscribe("f", 0)
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    global message
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    message = msg.payload
+    mqttc.publish("f2",msg.payload);
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+# Connect
+mqttc.connect("localhost", 1883,60)
+
+
+# Continue the network loop
+mqttc.loop_forever()
