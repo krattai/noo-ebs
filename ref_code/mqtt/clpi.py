49a09b544a167cc8c4891eb5b097e394c981b7d5
Kevin Rattai <krattai@gmail.com>
2016-01-27 20:05:36 -0600

    Modified channel and broker and response msg

--- ref_code/mqtt/clpi.py
+++ ref_code/mqtt/clpi.py
@@ -6,19 +6,23 @@ import paho.mqtt.client as mqtt
 
 # reference to MQTT python found here: http://mosquitto.org/documentation/python/
 
-# requires:  pip install paho-mqtt
+# requires:  sudo pip install paho-mqtt
 # pip requires: sudo apt-get install python-pip
 
 message = 'ON'
 def on_connect(mosq, obj, rc):
-    mqttc.subscribe("aebl/hello", 0)
+#    mqttc.subscribe("aebl/hello", 0)
+#    mqttc.subscribe("aebl/alive", 0)
+    mqttc.subscribe("uvea/alive", 0)
     print("rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
     global message
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     message = msg.payload
-    mqttc.publish("uvea/world",msg.payload);
+#    mqttc.publish("uvea/world",msg.payload);
+    mqttc.publish("uvea/world","uvea/alive - ACK");
+#    mqttcb.publish("uvea/world",msg.payload);
 
 def on_publish(mosq, obj, mid):
     print("mid: " + str(mid))
@@ -35,9 +39,14 @@ mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
+
 # Connect
 # mqttc.connect("localhost", 1883,60)
+
 mqttc.connect("2001:5c0:1100:dd00:240:63ff:fefd:d3f1", 1883,60)
+# mqttc.connect("2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7", 1883,60)
+
+# mosquitto_sub -h 2001:5c0:1100:dd00:ba27:ebff:fe2c:41d7 -t "hello/+" -t "aebl/+" -t "ihdn/+" -t "uvea/+"
 
 # Continue the network loop
 mqttc.loop_forever()
