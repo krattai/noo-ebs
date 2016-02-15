a50d4e9186d0058c787bc0b6a201bc0d7e7a3e93
Kevin Rattai <krattai@gmail.com>
2016-02-15 15:41:17 -0600

    Updated pub reference as IPv4 and IPv6

--- ref_code/mqtt/pub.sh
+++ ref_code/mqtt/pub.sh
@@ -9,8 +9,8 @@ ext_ip4=$(dig +short myip.opendns.com @resolver1.opendns.com)
 ext_ip6=$(curl icanhazip.com)
 # mosquitto_pub -d -t hello/world -m "$(date) : irot LdB, online. IP is $ext_ip" -h "uveais.ca"
 # mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn server IP $ext_ip is online." -h "2604:8800:100:19a::2"
-mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn device IP $ext_ip4 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
-mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn device IP $ext_ip6 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
+mosquitto_pub -d -t uvea/alive -m "$(date) : $hostn IPv4 $ext_ip4 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
+mosquitto_pub -d -t aebl/alive -m "$(date) : $hostn IPv6 $ext_ip6 is online." -h "2001:5c0:1100:dd00:240:63ff:fefd:d3f1"
 # i=$[$i+1]
 sleep 300
 done
