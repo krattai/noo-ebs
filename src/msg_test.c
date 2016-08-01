8b4f7449cd2d4123c264a54d33258965a3ee9a22
Kevin Rattai <krattai@gmail.com>
2016-08-01 07:30:31 -0500

    Added reference remarks on coding for paho mqtt

--- src/msg_test.c
+++ src/msg_test.c
@@ -6,6 +6,13 @@
  Version     : 0.0.001
  Copyright   : working license: The BSD 2-Clause License -> http://opensource.org/licenses/bsd-license.php
  Description : test program to check mqtt message processing
+
+ Important to note, in order to get certain aspects of this code working, using the paho mqtt client code for testing.
+ For tests, remember to include LIB path as shown in -> https://forums.aws.amazon.com/thread.jspa?threadID=218892
+
+ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD
+
+ And as perm solution, copy to /usr/lib
  ============================================================================
 */
 
